9c21463fddecacc4048e68ab96b6d11fb6566dc7
Estudianteucne <Estudianteucne@TERMINAL-B10>
2024-09-23 21:06:28 -0400

    Hice Registro

--- LiamellCruz_Ap1_P1/Service/RegistroService.cs
+++ LiamellCruz_Ap1_P1/Service/RegistroService.cs
@@ -1,5 +1,11 @@
-﻿namespace LiamellCruz_Ap1_P1.Service;
+﻿using LiamellCruz_Ap1_P1.DAL;
+
+namespace LiamellCruz_Ap1_P1.Service;
 
 public class RegistroService
 {
+    private readonly Contexto _contexto;
+
+
+
 }
