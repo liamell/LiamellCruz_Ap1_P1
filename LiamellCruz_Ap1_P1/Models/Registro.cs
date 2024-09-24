bb2401126909029a84bd67177dfaaf665e8a0546
Estudianteucne <Estudianteucne@TERMINAL-B10>
2024-09-23 20:51:12 -0400

    Hice el Modelo

---
+++ LiamellCruz_Ap1_P1/Models/Registro.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiamellCruz_Ap1_P1.Models;
+
+public class Registro
+{
+    [Key]
+    public int MyProperty2 { get; set; }
+    public int MyProperty { get; set; }
+
+    public int MyProperty1 { get; set; }
+
+}
