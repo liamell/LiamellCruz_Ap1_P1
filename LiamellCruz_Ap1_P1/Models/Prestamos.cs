9e8e5df7f9cf22b50de6c012af8093313632ae9a
Estudianteucne <Estudianteucne@TERMINAL-B10>
2024-09-30 19:22:27 -0400

    Hice el Modelo Prestamos

---
+++ LiamellCruz_Ap1_P1/Models/Prestamos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiamellCruz_Ap1_P1.Models;
+
+public class Prestamos
+{
+    [Key]
+    public int PrestamoId { get; set; }
+
+    [Required(ErrorMessage =" Campo obligatorio")]
+    [RegularExpression(@"[A-Za-z\s]+$", ErrorMessage= "Solo se permiten letras")]
+    public string? Deudor { get; set; }
+    [Required(ErrorMessage = " Campo obligatorio")]
+    [RegularExpression(@"[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string? Concepto { get; set; }
+    [Required(ErrorMessage = " Campo obligatorio")]
+  
+    public double Monto { get; set; }
+
+}
